d723d5c334e1939b5d49f2208dfe1fbd35647e60
lu.dong <WeiMob@201708>
2017-11-06 19:13:59 +0800

    退货单审核

--- src/main/java/com/fangxin/siwei/fangzhi/service/impl/audit/purchase/ReturnAuditCheckingServiceImpl.java
+++ src/main/java/com/fangxin/siwei/fangzhi/service/impl/audit/purchase/ReturnAuditCheckingServiceImpl.java
@@ -2,10 +2,10 @@ package com.fangxin.siwei.fangzhi.service.impl.audit.purchase;
 
 import com.fangxin.siwei.fangzhi.common.enums.AuditTypeEnum;
 import com.fangxin.siwei.fangzhi.common.result.Result;
-import com.fangxin.siwei.fangzhi.mapper.SwDeliverBaseMapper;
 import com.fangxin.siwei.fangzhi.mapper.SwPurchaseBaseMapper;
-import com.fangxin.siwei.fangzhi.modal.SwDeliverBase;
+import com.fangxin.siwei.fangzhi.mapper.SwReturnBaseMapper;
 import com.fangxin.siwei.fangzhi.modal.SwPurchaseBase;
+import com.fangxin.siwei.fangzhi.modal.SwReturnBase;
 import com.fangxin.siwei.fangzhi.service.audit.CurrentStageSearchServiceParam;
 import com.fangxin.siwei.fangzhi.service.audit.IAuditCheckingService;
 import org.springframework.stereotype.Service;
@@ -18,22 +18,22 @@ import javax.annotation.Resource;
  * @Description：
  **/
 @Service
-public class PurchaseAuditCheckingServiceImpl implements IAuditCheckingService {
+public class ReturnAuditCheckingServiceImpl implements IAuditCheckingService {
     @Resource
-    SwPurchaseBaseMapper swPurchaseBaseMapper;
+    SwReturnBaseMapper swReturnBaseMapper;
 
     @Override
     public AuditTypeEnum getAuditType() {
-        return AuditTypeEnum.PURCHASE;
+        return AuditTypeEnum.RETURN;
     }
 
     @Override
     public Result<String> checking(CurrentStageSearchServiceParam param) {
         Result _result=Result.newSuccess();
-        SwPurchaseBase swPurchaseBase=swPurchaseBaseMapper.selectByPurNo(param.getSourceNo());
+        SwReturnBase swReturnBase=swReturnBaseMapper.selectByReturnNo(param.getSourceNo());
         String status="";
-        if(swPurchaseBase!=null){
-            status=swPurchaseBase.getPurStatus();
+        if(swReturnBase!=null){
+            status=swReturnBase.getReturnStatus();
         }
         _result.setData(status);
         return _result;
