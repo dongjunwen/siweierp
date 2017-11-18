e42cb21b42a789079d7c645a49eaa3d0207cc867
lu.dong <WeiMob@201708>
2017-11-18 12:46:49 +0800

    解决跨域问题

--- src/main/java/com/fangxin/siwei/fangzhi/interceptor/MyInterceptor.java
+++ src/main/java/com/fangxin/siwei/fangzhi/interceptor/MyInterceptor.java
@@ -1,5 +1,7 @@
 package com.fangxin.siwei.fangzhi.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,19 +13,21 @@ import javax.servlet.http.HttpServletResponse;
  * Created by Administrator on 2017/8/1.
  */
 public class MyInterceptor implements HandlerInterceptor {
+    private static final Logger logger= LoggerFactory.getLogger(MyInterceptor.class);
+
     /**
      * 预处理回调方法，实现处理器的预处理（如登录检查），第三个参数为响应的处理器；
      * 返回值：true表示继续流程（如调用下一个拦截器或处理器）；
      * false表示流程中断（如登录检查失败），不会继续调用其他的拦截器或处理器，此时我们需要通过response来产生响应
      * @param request
-     * @param res
+     * @param resp
      * @param o
      * @return
      * @throws Exception
      */
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse res, Object o) throws Exception {
-
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse resp, Object o) throws Exception {
+        setResHeader(request,resp);
         return true;
     }
 
@@ -31,9 +35,11 @@ public class MyInterceptor implements HandlerInterceptor {
      * 设置请求头格式
      * @param res
      */
-    public void setResHeader(HttpServletResponse res){
+    public void setResHeader(HttpServletRequest request,HttpServletResponse res){
         res.setContentType("application/octet-stream"); //设置返回格式二进制流
-        res.setHeader("Access-Control-Allow-Origin", "*");
+        String reqPath=request.getServerName()+":"+request.getServerPort();
+        logger.debug("reqPath:{}",reqPath);
+        res.setHeader("Access-Control-Allow-Origin", reqPath);
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         res.setHeader("Access-Control-Max-Age", "0");
