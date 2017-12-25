4758543c61bcc5ade51269b5f92baebb252500d1
lu.dong <WeiMob@201708>
2017-12-25 18:01:02 +0800

    参数调整

--- src/main/java/com/fangxin/siwei/fangzhi/mapper/SysRoleResourceMapper.java
+++ src/main/java/com/fangxin/siwei/fangzhi/mapper/SysRoleResourceMapper.java
@@ -2,6 +2,7 @@ package com.fangxin.siwei.fangzhi.mapper;
 
 import com.fangxin.siwei.fangzhi.common.utils.MyMapper;
 import com.fangxin.siwei.fangzhi.modal.SysRoleResource;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,5 +11,5 @@ public interface SysRoleResourceMapper extends MyMapper<SysRoleResource>{
 
     List<SysRoleResource> selectByUserNo(String userNo);
 
-    SysRoleResource selectByRoleCodeAndSourceNo(String roleCode, String sourceNo);
+    SysRoleResource selectByRoleCodeAndSourceNo(@Param("roleCode") String roleCode, @Param("sourceNo")String sourceNo);
 }
